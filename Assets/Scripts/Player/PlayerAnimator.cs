ccce9b9b0b061d94d87f8081e94a3e8a2a5ea6e1
Mark Schafers <mark_schafers@hotmail.com>
2018-09-22 09:09:32 +0800

    First commit! Added tile palettes and tiles and created a basic starter area. Also create player character with basic movement and animation

---
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();  
+    }
+
+    public void Move(float horizontalValue)
+    {
+        anim.SetFloat("Move", Mathf.Abs(horizontalValue));
+    }
+
+    public void Jump(bool isJumping)
+    {
+        anim.SetBool("Jump", isJumping);
+    }
+}
